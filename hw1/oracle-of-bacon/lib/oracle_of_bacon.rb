8f5848c1d3e5dd6a1d39aac58d0972267400d1c4
bgr11n <bgr11n@gmail.com>
2014-06-17 21:29:26 +0300

    Part 2 done

--- hw1/oracle-of-bacon/lib/oracle_of_bacon.rb
+++ hw1/oracle-of-bacon/lib/oracle_of_bacon.rb
@@ -61,6 +61,12 @@ class OracleOfBacon
     def parse_response
       if ! @doc.xpath('/error').empty?
         parse_error_response
+      elsif ! @doc.xpath('/link').empty?
+        parse_graph_response
+      elsif ! @doc.xpath('/spellcheck').empty?
+        parse_spellcheck_response
+      else
+        parse_unknown_response
       # your code here: 'elsif' clauses to handle other responses
       # for responses not matching the 3 basic types, the Response
       # object should have type 'unknown' and data 'unknown response'         
@@ -69,6 +75,20 @@ class OracleOfBacon
     def parse_error_response
       @type = :error
       @data = 'Unauthorized access'
+    end
+    def parse_graph_response
+      @type = :graph
+      actors = @doc.xpath('//actor').map(&:text)
+      movies = @doc.xpath('//movie').map(&:text)
+      @data = actors.zip(movies).flatten.compact
+   end
+    def parse_spellcheck_response
+      @type = :spellcheck
+      @data = @doc.xpath('//match').map(&:text)
+    end
+    def parse_unknown_response
+      @type = :unknown
+      @data = 'unknown response type'
     end
   end
 end
